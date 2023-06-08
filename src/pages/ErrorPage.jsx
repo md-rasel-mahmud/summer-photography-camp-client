0b8fe1a33cfec3e5fab39837e815c247b701a494
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-06-08 06:08:03 +0600

    Added lottie animation in error pjage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,25 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from "react-router-dom";
+import Lottie from "lottie-react";
+import errorAnimation from "../assets/error-animation.json";
 
 const ErrorPage = () => {
-    const error = useRouteError()
-    console.log(error);
-    return (
-        <div className='text-center flex min-h-screen flex-col justify-center '>
-            <h1 className='text-4xl font-bold text-error'>{error.status}</h1>
-            <h2 className='text-2xl'>{error.data}</h2>
-        </div>
-    );
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div className="text-center flex min-h-screen flex-col justify-center ">
+      <div className="w-1/4 mx-auto">
+        <Lottie animationData={errorAnimation} />
+      </div>
+      <h2 className="text-2xl text-error mb-4">
+        {error.status} {error.data}
+      </h2>
+      <div className="text-center ">
+        <Link to={"/"} className="btn btn-accent">
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ErrorPage;
